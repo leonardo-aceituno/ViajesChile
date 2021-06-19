86f515f8a33a54ff1679c96b317a82c40487ce9f
Leo Aceituno <leonardo.aceituno@gmail.com>
2021-06-19 17:56:55 -0400

    ADD  - Html: destacados, contacto, footer | JS: smooth scrolling, ajustes menu, ocultar tarjetas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,53 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('h1').click(function (){
-        $('h1').css("color","pink");
+
+
+    // color al menu despues de la galeria
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > $("#quienes-somos").offset().top - 50) {
+            $("nav").addClass("color-fondo");
+        } else {
+            $("nav").removeClass("color-fondo");
+        };
+    });
+
+    // ocultar tarjetas
+    $("#tarjeta1").click(function () {
+        $("#txt1").toggle("slow")
+    })
+    $("#tarjeta2").click(function () {
+        $("#txt2").toggle("slow")
+    })
+    $("#tarjeta3").click(function () {
+        $("#txt3").toggle("slow")
+    })
+    $("#tarjeta4").click(function () {
+        $("#txt4").toggle("slow")
     })
 
-})
 
+    // smooth scrolling
+    $('a[href*="#"]').click(function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 200);
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+})
